175844bfe73da1f8b0c53d24771c3f7df3040d14
yanuar <yanuaraditia@outlook.com>
2023-02-11 10:05:24 +0700

    UPDATE

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   docus: {
     title: 'KiriminAja Developer',
     description: 'Develop. Connect. Shipped. Tenang Pakai #KiriminAja',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: '/cover.png',
     socials: {
       github: 'kiriminaja',
       instagram: 'kiriminaja.it'
