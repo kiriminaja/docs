490152e353ee9bd009ca4b455045bdcadfabb0ff
yanuar <yanuaraditia@outlook.com>
2023-02-13 10:00:06 +0700

    Update documentations

--- app.config.ts
+++ app.config.ts
@@ -1,34 +1,38 @@
 export default defineAppConfig({
-  docus: {
-    title: 'Docs',
-    description: 'Develop. Connect. Shipped. Tenang Pakai #KiriminAja',
-    image: '/cover.png',
-    socials: {
-      github: 'kiriminaja',
-      instagram: 'kiriminaja.it'
-    },
-    github: {
-      root: 'content',
-      edit: true,
-      contributors: false
-    },
-    aside: {
-      level: 1,
-      exclude: [
-          '/'
-      ]
-    },
-    header: {
-      logo: true,
-      showLinkIcon: true,
-      exclude: []
-    },
-    footer: {
-      credits: {
-        icon: '',
-        text: '© Powered by KiriminAja Tech',
-        href: 'https://instagram.com/kiriminaja.it',
-      }
+    docus: {
+        title: 'Docs',
+        description: 'Develop. Connect. Shipped. Tenang Pakai #KiriminAja',
+        image: '/cover.png',
+        socials: {
+            github: 'kiriminaja',
+            instagram: 'kiriminaja.it'
+        },
+        github: {
+            dir: '/content',
+            root: '/content',
+            edit: true,
+            releases: true,
+            owner: 'kiriminaja',
+            repo: 'docs',
+            branch: 'main'
+        },
+        aside: {
+            level: 1,
+            exclude: [
+                '/'
+            ]
+        },
+        header: {
+            logo: true,
+            showLinkIcon: true,
+            exclude: []
+        },
+        footer: {
+            credits: {
+                icon: '',
+                text: '© Powered by KiriminAja Tech',
+                href: 'https://instagram.com/kiriminaja.it',
+            }
+        }
     }
-  }
 })
